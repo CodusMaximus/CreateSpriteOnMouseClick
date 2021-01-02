fe0a6c3d24140ff8be5b5de2a22faf2b99e83d2e
Shaun <shaun@ivts.co.uk>
2021-01-02 14:57:23 +0000

    Create sprite on clicking

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour {
+
+    [SerializeField]
+    GameObject clickPrefab;
+
+    PhotonView photonView;
+    void Start() {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    void Update() {
+        if (!photonView.IsMine && PhotonNetwork.IsConnected) {
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+            Vector3 mouseInWorld = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Click(mouseInWorld);
+        }
+    }
+
+    void Click(Vector3 mousePosition) {
+        if (PhotonNetwork.IsConnected) {
+            PhotonNetwork.Instantiate("Click", mousePosition, Quaternion.identity);
+        }
+        else {
+            Instantiate(clickPrefab, mousePosition, Quaternion.identity);
+        }
+    }
+}
